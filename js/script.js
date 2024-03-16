1196f88da26afdb9f13cc0354968889940be9a1e
elmurat-karabekov <karabekov.elmurat@yandex.kz>
2024-03-16 12:11:20 +0500

    Implement movie-details page w/ packdrop image

--- js/script.js
+++ js/script.js
@@ -74,6 +74,79 @@ async function displayPopularShows() {
   });
 }
 
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchData(`movie/${movieId}`);
+
+  displayBackgroundImage('movie', movie.backdrop_path);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+      ${
+        movie.poster_path
+          ? `<img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                class="card-img-top"
+                alt="${movie.title}"
+              />`
+          : `<img
+                src="../images/no-image.jpg"
+                class="card-img-top"
+                alt="${movie.title}"
+              />`
+      }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${Math.round(movie.vote_average * 10) / 10} / 10
+        </p>
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        <p>
+          ${movie.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${
+          movie.homepage
+        }" target="_blank" class="btn">Visit Movie Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> $${movie.budget.toLocaleString()}</li>
+        <li><span class="text-secondary">Revenue:</span> $${movie.revenue.toLocaleString()}</li>
+        <li><span class="text-secondary">Runtime:</span> ${movie.runtime}</li>
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${movie.production_companies
+        .map((comp) => `<span>${comp.name}</span>`)
+        .join(', ')}</div>
+    </div>
+  `;
+
+  document.querySelector('#movie-details').appendChild(div);
+}
+
+function displayBackgroundImage(type, backdrop_path) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backdrop_path})`;
+  overlayDiv.classList.add('overlay');
+
+  type === 'movie'
+    ? document.querySelector('#movie-details').appendChild(overlayDiv)
+    : document.querySelector('#tv-details').appendChild(overlayDiv);
+}
+
 // Fetchin Data from TMDB API
 async function fetchData(endpoint) {
   const API_KEY = '3acd8b99e2a5040ad1e83e38858c7db3';
@@ -121,6 +194,7 @@ function init() {
       displayPopularShows();
       break;
     case '/movie-details.html':
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       break;
