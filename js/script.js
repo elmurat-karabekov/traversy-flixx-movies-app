7f79e7c99f8e5bc0ec06fa6771da14df910dca8e
elmurat-karabekov <karabekov.elmurat@yandex.kz>
2024-03-16 13:34:04 +0500

    Plugged in swiper library to display movies in theater

--- js/script.js
+++ js/script.js
@@ -214,6 +214,58 @@ function displayBackgroundImage(type, backdrop_path) {
     : document.querySelector('#show-details').appendChild(overlayDiv);
 }
 
+async function displaySlider() {
+  const { results } = await fetchData('movie/now_playing');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+
+    div.classList.add('swiper-slide');
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+      movie.title
+    }" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+          1
+        )} / 10
+      </h4>
+    `;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    sliderPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 // Fetchin Data from TMDB API
 async function fetchData(endpoint) {
   const API_KEY = '3acd8b99e2a5040ad1e83e38858c7db3';
@@ -255,6 +307,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
+      displaySlider();
       displayPopularMovies();
       break;
     case '/shows.html':
